bcd6847ef7e173f0581bc71cb4cab36a37bf3947
Xusheng <jsksxs360@163.com>
2017-09-22 20:39:43 +0800

    fix tab & space problem

--- src/test/demo/ConvertDemo.java
+++ src/test/demo/ConvertDemo.java
@@ -5,10 +5,10 @@ import me.xiaosheng.chnlp.AHANLP;
 public class ConvertDemo {
 
     public static void main(String[] args) {
-	    //简繁转换
-	    String tc = AHANLP.convertSC2TC("用笔记本电脑写程序");
-	    System.out.println(tc);
-	    String sc = AHANLP.convertTC2SC("「以後等妳當上皇后，就能買士多啤梨慶祝了」");
-	    System.out.println(sc);
+        //简繁转换
+        String tc = AHANLP.convertSC2TC("用笔记本电脑写程序");
+        System.out.println(tc);
+        String sc = AHANLP.convertTC2SC("「以後等妳當上皇后，就能買士多啤梨慶祝了」");
+        System.out.println(sc);
     }
 }
--- src/test/demo/ParserDemo.java
+++ src/test/demo/ParserDemo.java
@@ -13,24 +13,24 @@ import me.xiaosheng.chnlp.parser.DependencyParser;
 public class ParserDemo {
 
     public static void main(String[] args) {
-	    
-	    String sentence = "北京是中国的首都";
-	    
-	    CoNLLSentence zhDeps = AHANLP.DependencyParse(sentence);
-	    
-	    // 直接遍历
-	    System.out.println("\n中文标签 --->");
-	    for (CoNLLWord dep : zhDeps)
-	        System.out.printf("%s --(%s)--> %s\n", dep.LEMMA, dep.DEPREL, dep.HEAD.LEMMA);
-	    
-	    // 以数组方式遍历
-	    System.out.println("\n遍历结果数组:");
-	    CoNLLWord[] wordArray = zhDeps.getWordArray();
+        
+        String sentence = "北京是中国的首都";
+        
+        CoNLLSentence zhDeps = AHANLP.DependencyParse(sentence);
+        
+        // 直接遍历
+        System.out.println("\n中文标签 --->");
+        for (CoNLLWord dep : zhDeps)
+            System.out.printf("%s --(%s)--> %s\n", dep.LEMMA, dep.DEPREL, dep.HEAD.LEMMA);
+        
+        // 以数组方式遍历
+        System.out.println("\n遍历结果数组:");
+        CoNLLWord[] wordArray = zhDeps.getWordArray();
         for (int i = 0; i < wordArray.length; i++) {
             CoNLLWord word = wordArray[i];
             System.out.printf("%s --(%s)--> %s\n", word.LEMMA, word.DEPREL, word.HEAD.LEMMA);
         }
-	    
+        
         // 从某个节点一路遍历到虚根
         CoNLLWord head = wordArray[2];
         System.out.println("\n从“" + head.LEMMA + "”遍历到虚根的路径为:");
@@ -60,17 +60,17 @@ public class ParserDemo {
         System.out.println(words);
         
         // 英文标签
-	    CoNLLSentence enDeps = AHANLP.DependencyParse(sentence, true);
-	    System.out.println("\n英文标签 --->");
-	    for (CoNLLWord dep : enDeps)
-	        System.out.printf("%s --(%s)--> %s\n", dep.LEMMA, dep.DEPREL, dep.HEAD.LEMMA);
-	    
-	    //自定义分词
-	    String sentence2 = "金正男在马来西亚被刺杀";
-	    List<Term> regResult = AHANLP.NLPSegment(sentence2);
-	    System.out.println("\nSeg Result: " + regResult);
-	    CoNLLSentence Deps = DependencyParser.parse(regResult);
-	    for (CoNLLWord dep : Deps)
-	        System.out.printf("%s --(%s)--> %s\n", dep.LEMMA, dep.DEPREL, dep.HEAD.LEMMA);
+        CoNLLSentence enDeps = AHANLP.DependencyParse(sentence, true);
+        System.out.println("\n英文标签 --->");
+        for (CoNLLWord dep : enDeps)
+            System.out.printf("%s --(%s)--> %s\n", dep.LEMMA, dep.DEPREL, dep.HEAD.LEMMA);
+        
+        //自定义分词
+        String sentence2 = "金正男在马来西亚被刺杀";
+        List<Term> regResult = AHANLP.NLPSegment(sentence2);
+        System.out.println("\nSeg Result: " + regResult);
+        CoNLLSentence Deps = DependencyParser.parse(regResult);
+        for (CoNLLWord dep : Deps)
+            System.out.printf("%s --(%s)--> %s\n", dep.LEMMA, dep.DEPREL, dep.HEAD.LEMMA);
     }
 }
